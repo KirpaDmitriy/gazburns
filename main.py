ee501a6217937b64ade3bd06dd2b985e80e7166b
KirpaDmitriy <dikiydiamond@yandex.ru>
2024-06-09 23:49:22 +0300

    Fix methods + add mocks

--- main.py
+++ main.py
@@ -11,7 +11,11 @@ app = FastAPI()
 MEGA_URL = "https://steamuserimages-a.akamaihd.net/ugc/862857581989807965/89518896478AB5FC4C81035678DB2B441ACE107A/?imw=512&imh=384&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true"
 general_case = Case(
     id=str(uuid.uuid4()),
-    images=[{"id": "0-1", "src": MEGA_URL}],
+    images=[
+        {"id": "0-1", "src": MEGA_URL},
+        {"id": "0-1", "src": MEGA_URL},
+        {"id": "0-1", "src": MEGA_URL},
+    ],
     meta_information={
         "width": 100,
         "height": 100,
@@ -19,9 +23,9 @@ general_case = Case(
 )
 
 
-@app.post("/cases", response_model=list[Case])
+@app.get("/cases", response_model=list[Case])
 async def cases():
-    return [general_case]
+    return [general_case, general_case, general_case]
 
 
 @app.post("/generate", response_model=Case)
@@ -35,6 +39,6 @@ async def add_text(params: TextParams):
     return general_case
 
 
-@app.post("/generate_text", response_model=GeneratedFormText)
+@app.get("/generate_text", response_model=GeneratedFormText)
 async def generate_text(case_id: str):
     return GeneratedFormText(title="Собака", subtitle="Улыбака")
