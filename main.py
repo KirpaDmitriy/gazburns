1ccaf6192ece1a9f26b9d468baff83f9da968d81
KirpaDmitriy <dikiydiamond@yandex.ru>
2024-06-11 23:35:03 +0300

    Fix cors

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import Depends, FastAPI, Header, HTTPException, status
+from fastapi.middleware.cors import CORSMiddleware
 
 from handlers.access import router as access_router
 from handlers.history import router as history_router
@@ -6,6 +7,15 @@ from handlers.images import router as images_router
 from handlers.text import router as text_router
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(access_router)
