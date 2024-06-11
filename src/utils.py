990397a1193aa849eb4ad051761942f6f62c14b0
KirpaDmitriy <dikiydiamond@yandex.ru>
2024-06-11 22:37:30 +0300

    Fix

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,6 @@ from models import Case, GenerationParams, general_case
 from src.images_generation import generate_image
 
 
-def extract_case(params: GenerationParams) -> Case:
+async def extract_case(params: GenerationParams) -> Case:
     await generate_image(params.audience, params.product)
     return general_case
