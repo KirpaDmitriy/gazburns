163ecdecae06e49fee46e56779bcd25a8267b453
KirpaDmitriy <dikiydiamond@yandex.ru>
2024-06-14 02:59:35 +0300

    Add primary key

--- database/tables.py
+++ database/tables.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String
+from sqlalchemy import Column, Integer, Sequence, String
 
 from .connection import BaseTable
 
@@ -6,5 +6,12 @@ from .connection import BaseTable
 class User(BaseTable):
     __tablename__ = "user"
 
+    id = Column(
+        Integer,
+        Sequence("auth_user_id_seq"),
+        primary_key=True,
+        nullable=False,
+        index=True,
+    )
     username = Column(String, unique=True, nullable=False)
     hashed_password = Column(String, nullable=False)
