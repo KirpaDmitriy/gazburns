e97f6fa01ca365e6aad8e3efc90ce207c6fe9ada
KirpaDmitriy <dikiydiamond@yandex.ru>
2024-06-14 03:17:13 +0300

    Fix

--- database/__init__.py
+++ database/__init__.py
@@ -18,12 +18,13 @@ async def get_user(username: str) -> dict:
                 )
             )
         ).one_or_none()
+
         print(
-            f"""Kek: {(
+            f"""Kek: {[id_[0] for id_ in (
             await session.execute(
                 select(User.username, User.hashed_password)
             )
-        ).scalars()}"""
+        ).scalars().all()]}"""
         )
         if not selected_user:
             return {}
