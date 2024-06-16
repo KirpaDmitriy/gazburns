91f076d610e90106744c8ae5fe4548d61f5cb205
KirpaDmitriy <dikiydiamond@yandex.ru>
2024-06-16 16:22:27 +0300

    Add router

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import Depends, FastAPI, Header, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
 
 from handlers.access import router as access_router
+from handlers.files_server import router as files_router
 from handlers.history import router as history_router
 from handlers.images import router as images_router
 from handlers.text import router as text_router
@@ -21,3 +22,4 @@ app.include_router(access_router)
 app.include_router(history_router)
 app.include_router(images_router)
 app.include_router(text_router)
+app.include_router(files_router)
