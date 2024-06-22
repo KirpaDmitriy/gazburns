9ff013500564527b9b0cecb3f6e34ca2df4d9df7
KirpaDmitriy <dikiydiamond@yandex.ru>
2024-06-22 20:39:29 +0300

    Check memory

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import Depends, FastAPI, Header, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
 
-# from handlers.access import router as access_router
+from handlers.access import router as access_router
 from handlers.files_server import router as files_router
 # from handlers.history import router as history_router
 from handlers.images import router as images_router
@@ -21,7 +21,7 @@ app.add_middleware(
 )
 
 
-# app.include_router(access_router)
+app.include_router(access_router)
 # app.include_router(history_router)
 app.include_router(images_router)
 # app.include_router(slogans_router)
