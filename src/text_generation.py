665858ff8b68cc662b065d9a28e53b113571ca61
KirpaDmitriy <dikiydiamond@yandex.ru>
2024-06-22 20:57:46 +0300

    Check memory

--- src/text_generation.py
+++ src/text_generation.py
@@ -5,7 +5,7 @@ import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 model = AutoModelForCausalLM.from_pretrained(
-    "Qwen/Qwen2-7B-Instruct", torch_dtype="auto", device_map="auto"
+    "Qwen/Qwen2-7B-Instruct", torch_dtype="auto"  # , device_map="auto"
 ).to("cuda")
 
 tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2-7B-Instruct")
