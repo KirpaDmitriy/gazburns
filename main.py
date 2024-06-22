83ced37a943b159f5623c6adcfba8e3fb6719cdf
KirpaDmitriy <dikiydiamond@yandex.ru>
2024-06-22 20:38:04 +0300

    Check memory

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import Depends, FastAPI, Header, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
 
 # from handlers.access import router as access_router
-# from handlers.files_server import router as files_router
+from handlers.files_server import router as files_router
 # from handlers.history import router as history_router
 from handlers.images import router as images_router
 # from handlers.slogans import router as slogans_router
