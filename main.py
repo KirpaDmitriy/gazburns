641a9b7745504605f7f423638abebc1c7b8f7cda
KirpaDmitriy <dikiydiamond@yandex.ru>
2024-06-22 13:55:30 +0300

    Add router

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from handlers.access import router as access_router
 from handlers.files_server import router as files_router
 from handlers.history import router as history_router
 from handlers.images import router as images_router
+from handlers.slogans import router as slogans_router
 from src.logger import app_logger
 
 log = app_logger(__name__)
@@ -23,4 +24,5 @@ app.add_middleware(
 app.include_router(access_router)
 app.include_router(history_router)
 app.include_router(images_router)
+app.include_router(slogans_router)
 app.include_router(files_router)
